7c14772ea5f306867b243a2c61792502978a0b9c
Seif Soliman <byseif21@gmail.com>
2025-05-03 18:23:42 +0300

    factor: authentication backend (#7)
    * factor: authentication backend
    
    * registeration and login models
    
    * password related models
    
    * add email sender service

--- Program.cs
+++ Program.cs
@@ -14,6 +14,9 @@ builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.Requ
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
+// Register email sender service
+builder.Services.AddTransient<Microsoft.AspNetCore.Identity.UI.Services.IEmailSender, Styleza.Services.EmailSender>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
