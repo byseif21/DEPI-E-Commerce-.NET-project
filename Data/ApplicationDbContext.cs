4defe5ac272e2b4ea0f42b7a487961fcbbcf974e
Seif Soliman <byseif21@gmail.com>
2025-05-12 22:18:51 +0300

    fix: incomplete EF setup for authentication system (#10)
    * missing EF packages
    
    * add validation for existing accounts during registration, and modify the registration flow to redirect to login instead of auto-login
    
    * temporary disable the email confirmation requirement for login
    
    * fix ApplicationDbContext to ensure Identity tables are created

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,10 +1,11 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Styleza.Models;
 
 namespace Styleza.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
